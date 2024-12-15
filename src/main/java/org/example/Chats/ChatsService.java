f1744f1d567cf0b28503d9f02e0662752cdfa452
aageev <Qwerty123$>
2024-12-15 13:18:35 +0300

    fix null chat title check

--- src/main/java/org/example/Chats/ChatsService.java
+++ src/main/java/org/example/Chats/ChatsService.java
@@ -5,7 +5,8 @@ import org.telegram.telegrambots.meta.api.objects.Chat;
 public class ChatsService {
     ChatsRepository repo = new ChatsRepository();
     public boolean checkChat(Long chatID) {
-        return repo.getChatTitleByID(chatID) != null;
+        Chat chat = repo.getChatByID(chatID);
+        return chat.getId() != null;
     }
     public void addChat(Chat chat) {
         repo.insertChatInDB(chat);
