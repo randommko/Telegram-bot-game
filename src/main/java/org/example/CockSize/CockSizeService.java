5cb5a4e6463fa2edaaa6b06c87d2fe776ce777f3
aageev <Qwerty123$>
2024-12-19 10:53:56 +0300

    new cocksize algorithm

--- src/main/java/org/example/CockSize/CockSizeService.java
+++ src/main/java/org/example/CockSize/CockSizeService.java
@@ -2,12 +2,16 @@ package org.example.CockSize;
 
 import org.example.Users.UsersService;
 
+import java.util.ArrayList;
 import java.util.Random;
 import com.vdurmont.emoji.EmojiParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class CockSizeService {
     private final CockSizeRepository repo = new CockSizeRepository();
+    private static final Logger logger = LoggerFactory.getLogger(CockSizeService.class);
     private UsersService usersService = new UsersService();
     public Integer measureCockSize(Long userID) {
         int newRandomSize = getCockSize();
@@ -20,51 +24,112 @@ public class CockSizeService {
     }
 
     private static int getCockSize() {
-        //TODO: изменить алгоритм, нет смысла в членах от 25 до 45, от 15 до 20
-        int randomNum = new Random().nextInt(100);        //Выбираем случайное число для попадания в распределение
+        ArrayList<Integer> cockSizeList = new ArrayList<>();
 
-        if (randomNum < 2)                                      //У 2% выборки
-            return new Random().nextInt(3);              //длина от 0 до 3
+        cockSizeList.add(0);
+        cockSizeList.add(1);
+        cockSizeList.add(2);
+        cockSizeList.add(3);
+        for (int i = 0; i < 2; i++) {
+            cockSizeList.add(5);
+        }
+        for (int i = 0; i < 2; i++) {
+            cockSizeList.add(6);
+        }
+        for (int i = 0; i < 2; i++) {
+            cockSizeList.add(7);
+        }
+        for (int i = 0; i < 2; i++) {
+            cockSizeList.add(8);
+        }
 
-        if (randomNum < 7)                                     //У 5% выборки
-            return new Random().nextInt(5) + 3;           //длина от 3 до 8
+        for (int i = 0; i < 5; i++) {
+            cockSizeList.add(10);
+        }
 
-        if (randomNum < 25)                                     //У 18% выборки
-            return new Random().nextInt(7) + 8;           //длина от 8 до 15
+        for (int i = 0; i < 5; i++) {
+            cockSizeList.add(11);
+        }
+        for (int i = 0; i < 5; i++) {
+            cockSizeList.add(13);
+        }
+        for (int i = 0; i < 4; i++) {
+            cockSizeList.add(15);
+        }
+        for (int i = 0; i < 4; i++) {
+            cockSizeList.add(16);
+        }
 
-        if (randomNum < 55)                                     //У 30% выборки
-            return new Random().nextInt(5) + 15;          //длина от 15 до 20
+        for (int i = 0; i < 2; i++) {
+            cockSizeList.add(20);
+        }
+        for (int i = 0; i < 2; i++) {
+            cockSizeList.add(24);
+        }
 
-        if (randomNum < 65)                                    //У 10% выборки
-            return new Random().nextInt(5) + 20;        //длина от 20 до 25
+        for (int i = 0; i < 1; i++) {
+            cockSizeList.add(40);
+        }
 
-        if (randomNum < 73)                                    //У 8% выборки
-            return new Random().nextInt(5) + 25;        //длина от 25 до 30
+        for (int i = 0; i < 1; i++) {
+            cockSizeList.add(45);
+        }
+        for (int i = 0; i < 1; i++) {
+            cockSizeList.add(49);
+        }
 
-        if (randomNum < 78)                                    //У 5% выборки
-            return new Random().nextInt(3) + 25;        //длина от 25 до 28
+        logger.info(cockSizeList.toString());
 
-        if (randomNum < 83)                                    //У 5% выборки
-            return new Random().nextInt(3) + 28;        //длина от 25 до 31
+        // Генерируем случайный элемент
+        Random random = new Random();
+        int randomIndex = random.nextInt(cockSizeList.size());
+        return cockSizeList.get(randomIndex);
 
-        if (randomNum < 88)                                    //У 5% выборки
-            return new Random().nextInt(3) + 31;        //длина от 31 до 34
+//        int randomNum = new Random().nextInt(100);        //Выбираем случайное число для попадания в распределение
+//
+//        if (randomNum < 2)                                      //У 2% выборки
+//            return new Random().nextInt(3);              //длина от 0 до 3
+//
+//        if (randomNum < 7)                                     //У 5% выборки
+//            return new Random().nextInt(5) + 3;           //длина от 3 до 8
+//
+//        if (randomNum < 25)                                     //У 18% выборки
+//            return new Random().nextInt(7) + 8;           //длина от 8 до 15
+//
+//        if (randomNum < 55)                                     //У 30% выборки
+//            return new Random().nextInt(5) + 15;          //длина от 15 до 20
+//
+//        if (randomNum < 65)                                    //У 10% выборки
+//            return new Random().nextInt(5) + 20;        //длина от 20 до 25
+//
+//        if (randomNum < 73)                                    //У 8% выборки
+//            return new Random().nextInt(5) + 25;        //длина от 25 до 30
+//
+//        if (randomNum < 78)                                    //У 5% выборки
+//            return new Random().nextInt(3) + 25;        //длина от 25 до 28
+//
+//        if (randomNum < 83)                                    //У 5% выборки
+//            return new Random().nextInt(3) + 28;        //длина от 25 до 31
+//
+//        if (randomNum < 88)                                    //У 5% выборки
+//            return new Random().nextInt(3) + 31;        //длина от 31 до 34
+//
+//        if (randomNum < 91)                                    //У 3% выборки
+//            return new Random().nextInt(3) + 34;        //длина от 34 до 37
+//
+//        if (randomNum < 94)                                    //У 3% выборки
+//            return new Random().nextInt(3) + 37;        //длина от 37 до 40
+//
+//        if (randomNum < 96)                                    //У 2% выборки
+//            return new Random().nextInt(3) + 40;        //длина от 40 до 43
+//
+//        if (randomNum < 98)                                    //У 2% выборки
+//            return new Random().nextInt(5) + 40;        //длина от 40 до 45
+//
+//        if (randomNum < 100)                                    //У 2% выборки
+//            return new Random().nextInt(5) + 45;        //длина от 45 до 50
+//        return -1;
 
-        if (randomNum < 91)                                    //У 3% выборки
-            return new Random().nextInt(3) + 34;        //длина от 34 до 37
-
-        if (randomNum < 94)                                    //У 3% выборки
-            return new Random().nextInt(3) + 37;        //длина от 37 до 40
-
-        if (randomNum < 96)                                    //У 2% выборки
-            return new Random().nextInt(3) + 40;        //длина от 40 до 43
-
-        if (randomNum < 98)                                    //У 2% выборки
-            return new Random().nextInt(5) + 40;        //длина от 40 до 45
-
-        if (randomNum < 100)                                    //У 2% выборки
-            return new Random().nextInt(5) + 45;        //длина от 45 до 50
-        return -1;
     }
 
     public String phraseSelection(int size, String username) {
