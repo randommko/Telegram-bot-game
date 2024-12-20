40906a98ba2533942c6baa4b7d834f206954e8fe
aageev <Qwerty123$>
2024-12-20 16:44:21 +0300

    Horoscope MVP done

--- src/main/java/org/example/Horoscope/Horoscope.java
+++ src/main/java/org/example/Horoscope/Horoscope.java
@@ -1,92 +1,32 @@
 package org.example.Horoscope;
 
 import com.fasterxml.jackson.annotation.JsonAnySetter;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.dataformat.xml.XmlMapper;
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
-aimport com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
-import org.example.TelegramBot;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.fasterxml.jackson.dataformat.xml.annotation.*;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.HashMap;
 import java.util.Map;
 
 @JacksonXmlRootElement(localName = "horo")
 public class Horoscope {
-    private static final String URL = "https://ignio.com/r/export/utf/xml/daily/com.xml";
-
-    private static final Logger logger = LoggerFactory.getLogger(Horoscope.class);
-    private Horoscope horoscope;
-    private final TelegramBot bot;
+    private Map<String, HoroscopeText> horoscopeTextMap = new HashMap<>();
+    //key - код знака зодиака
+    //value - текст для всех дней
     @JacksonXmlProperty(localName = "date")
     private DateInfo dateInfo;
-    private final Map<String, Zodiac> signsMap = new HashMap<>();
-
     @JsonAnySetter
-    public void addSign(String key, Zodiac value) {
-        signsMap.put(key, value);
+    public void addSign(String key, HoroscopeText value) {
+        //key - код знака зодиака
+        //value - текст гороскопа на этот день
+        horoscopeTextMap.put(key, value);
     }
     public DateInfo getDateInfo() {
         return dateInfo;
     }
-
     public void setDateInfo(DateInfo dateInfo) {
         this.dateInfo = dateInfo;
     }
-
-    public Map<String, Zodiac> getSignsMap() {
-        return signsMap;
+    public Map<String, HoroscopeText> getHoroscopeTextMap() {
+        return horoscopeTextMap;
     }
 
-    public Horoscope() {
-        bot = TelegramBot.getInstance();
-    }
-
-    public String getHoroscope () {
-        try {
-            // Создаем XmlMapper
-            XmlMapper xmlMapper = new XmlMapper();
-
-            // Парсим XML в объект Horoscope
-            horoscope = xmlMapper.readValue(getHoroscopeXML(), Horoscope.class);
-
-            StringBuilder result = new StringBuilder();
-            result.append("Гороскоп на ").append(horoscope.dateInfo.getToday()).append("\n");
-            for (Map.Entry<String, Zodiac> entry : horoscope.signsMap.entrySet()) {
-                result.append(entry.getKey()).append(": ").append(entry.getValue().getTodayHoroscope()).append("\n");
-            }
-            return result.toString();
-
-        } catch (Exception e) {
-            logger.debug("Ошибка парсинга гороскопа" + e);
-            return null;
-        }
-    }
-
-    public static String getHoroscopeXML() throws IOException, InterruptedException {
-        // Создаем клиент
-        HttpClient client = HttpClient.newHttpClient();
-
-        // Создаем запрос
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(URL))
-                .GET()
-                .build();
-
-        // Отправляем запрос и получаем ответ
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-
-        // Проверяем код ответа
-        if (response.statusCode() == 200) {
-            return response.body(); // Возвращаем тело ответа
-        } else {
-            throw new IOException("Ошибка при получении данных: " + response.statusCode());
-        }
-    }
 }
